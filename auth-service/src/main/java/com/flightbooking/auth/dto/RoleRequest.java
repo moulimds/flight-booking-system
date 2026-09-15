7044cd436d91f46955ae8fc4ad4005a75dafcfaa
Abinesh S <abinesh632005@gmail.com>
2026-09-15 20:20:49 +0530

    Delete auth-service directory

--- auth-service/src/main/java/com/flightbooking/auth/dto/RoleRequest.java
+++
@@ -1,6 +0,0 @@
-package com.flightbooking.auth.dto;
-
-import com.flightbooking.auth.model.Role;
-import jakarta.validation.constraints.NotNull;
-
-public record RoleRequest(@NotNull Role role) {}
--- auth-service/src/main/java/com/flightbooking/auth/dto/StatusRequest.java
+++
@@ -1,5 +0,0 @@
-package com.flightbooking.auth.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-public record StatusRequest(@NotNull Boolean active) {}
