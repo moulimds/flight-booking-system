f9b17b9eaea787183c65ff3edb65edd1b1b51952
Abinesh S <abinesh632005@gmail.com>
2026-09-16 15:09:15 +0530

    Delete auth-service directory

--- auth-service/src/main/java/com/flightbooking/auth/dto/ChangePasswordRequest.java
+++
@@ -1,9 +0,0 @@
-package com.flightbooking.auth.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-public record ChangePasswordRequest(
-    @NotBlank String oldPassword,
-    @NotBlank @Size(min = 6, max = 100) String newPassword
-) {}
