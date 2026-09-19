5d15875ded947c30a4f66040dbe305e4239aa557
Mohammed Ashad <ashadlo225@gmail.com>
2026-09-19 16:05:19 +0530

    Delete previous seat-service files

--- seat-service/src/main/java/com/airline/seat/entity/SeatClass.java
+++
@@ -1,7 +0,0 @@
-package com.airline.seat.entity;
-
-public enum SeatClass {
-    ECONOMY,
-    BUSINESS,
-    FIRST
-}
--- seat-service/src/main/java/com/airline/seat/exception/InvalidSeatOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.airline.seat.exception;
-
-public class InvalidSeatOperationException extends RuntimeException {
-    public InvalidSeatOperationException(String message) {
-        super(message);
-    }
-}
