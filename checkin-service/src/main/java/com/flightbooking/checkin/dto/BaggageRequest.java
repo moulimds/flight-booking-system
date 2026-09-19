ab30d7d49fe45494a38652ef6377d0239b6b3166
Abinesh S <abinesh632005@gmail.com>
2026-09-19 15:38:45 +0530

    Delete checkin-service directory

--- checkin-service/src/main/java/com/flightbooking/checkin/dto/BaggageRequest.java
+++
@@ -1,9 +0,0 @@
-package com.flightbooking.checkin.dto;
-
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-
-public record BaggageRequest(
-    @NotNull @Min(0) Integer baggageCount,
-    @NotNull @Min(0) Double baggageWeight
-) {}
