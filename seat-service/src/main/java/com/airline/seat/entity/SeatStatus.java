632d393cbaac505faee31ba602070b920ce8a3e6
Mohammed Ashad <ashadlo225@gmail.com>
2026-09-19 16:17:38 +0530

    Wipe eureka branch clean

--- seat-service/src/main/java/com/airline/seat/entity/SeatStatus.java
+++
@@ -1,8 +0,0 @@
-package com.airline.seat.entity;
-
-public enum SeatStatus {
-    AVAILABLE,
-    HELD,
-    RESERVED,
-    BLOCKED
-}
--- seat-service/src/main/java/com/airline/seat/exception/SeatUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package com.airline.seat.exception;
-
-public class SeatUnavailableException extends RuntimeException {
-    public SeatUnavailableException(String message) {
-        super(message);
-    }
-}
