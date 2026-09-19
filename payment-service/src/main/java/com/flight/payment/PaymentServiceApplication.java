34100db143175b5cb8f570b4a55df499f9f3fda8
Mouli M <155385926+moulimds@users.noreply.github.com>
2026-09-19 18:33:44 +0530

    Remove ownership comment from PaymentServiceApplication
    Removed the ownership comment from PaymentServiceApplication.

--- payment-service/src/main/java/com/flight/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/flight/payment/PaymentServiceApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * Payment Service - Owned by MONI
- */
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PaymentServiceApplication {
