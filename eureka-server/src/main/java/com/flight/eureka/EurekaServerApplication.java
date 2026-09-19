71b169859d970e0241d41eedda773609838d2fbf
Mohammed Ashad <ashadlo225@gmail.com>
2026-09-19 18:20:04 +0530

    Add updated eureka-server

---
+++ eureka-server/src/main/java/com/flight/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.flight.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Service Registry - Owned by ASHWAD
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
