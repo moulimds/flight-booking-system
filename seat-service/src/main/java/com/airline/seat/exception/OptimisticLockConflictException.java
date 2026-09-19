44a17dd378216fd854b3344d770a50317744e818
Mohammed Ashad <ashadlo225@gmail.com>
2026-09-19 16:14:20 +0530

    Remove seat-service from notification branch

--- seat-service/src/main/java/com/airline/seat/exception/OptimisticLockConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.airline.seat.exception;
-
-public class OptimisticLockConflictException extends RuntimeException {
-    public OptimisticLockConflictException(String message) {
-        super(message);
-    }
-}
